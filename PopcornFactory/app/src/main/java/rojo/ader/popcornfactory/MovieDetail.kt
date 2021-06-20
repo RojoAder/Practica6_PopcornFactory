0424be2e0f32d9b41bd7c2722b5a68205a296c3f
RojoAder <alexrojo6@hotmail.com>
2021-06-19 19:19:22 -0700

    Terminado

--- PopcornFactory/app/src/main/java/rojo/ader/popcornfactory/MovieDetail.kt
+++ PopcornFactory/app/src/main/java/rojo/ader/popcornfactory/MovieDetail.kt
@@ -2,10 +2,20 @@ package rojo.ader.popcornfactory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class MovieDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
+
+        val imageView : ImageView = findViewById(R.id.movie_image_detail)
+        val titulo : TextView = findViewById(R.id.movie_title_detail)
+        val detail: TextView = findViewById(R.id.movie_text_detail)
+
+        titulo.setText(intent.getStringExtra("titulo"))
+        imageView.setImageResource(intent.getIntExtra("header",0))
+        detail.setText(intent.getStringExtra("sinopsis"))
     }
 }
