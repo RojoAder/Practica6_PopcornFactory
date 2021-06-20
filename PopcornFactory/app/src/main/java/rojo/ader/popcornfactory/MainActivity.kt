e6e240131859dd0582d6555f6a4255db705c84a3
RojoAder <alexrojo6@hotmail.com>
2021-06-19 18:28:41 -0700

    Casi terminada

--- PopcornFactory/app/src/main/java/rojo/ader/popcornfactory/MainActivity.kt
+++ PopcornFactory/app/src/main/java/rojo/ader/popcornfactory/MainActivity.kt
@@ -1,11 +1,20 @@
 package rojo.ader.popcornfactory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button:Button = findViewById(R.id.getmeIn_button)
+
+        button.setOnClickListener {
+            var intent:Intent = Intent(this,CatalogActivity::class.java)
+            this.startActivity(intent)
+        }
     }
 }
---
+++ PopcornFactory/app/src/main/java/rojo/ader/popcornfactory/Pelicula.kt
@@ -0,0 +1,3 @@
+package rojo.ader.popcornfactory
+
+data class Pelicula(var titulo:String, var imagen: Int, var header:Int, var sinopsis:String)
